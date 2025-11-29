6b4fcc9f293dc11914da6298e8a1a9f5db0ffc7d
Shashwat Shekhar <shashwatshekhar646@gmail.com>
2025-11-29 21:49:30 +0530

    Final commit

--- dashboard.py
+++ dashboard.py
@@ -21,7 +21,7 @@ st.title("🦎 CHAMELEON: ACTIVE DECEPTION SYSTEM")
 
 # Heartbeat Check
 try:
-    r = requests.get("http://127.0.0.1:8000/", timeout=1)
+    r = requests.get("http://127.0.0.1:8000/", timeout=2)
     status_html = f'<div class="status-box success">CLOUD STATUS: 🟢 OPERATIONAL | BACKEND: ONLINE</div>'
 except:
     status_html = f'<div class="status-box error">CLOUD STATUS: 🔴 BOOTING DEFENSE ENGINE...</div>'
